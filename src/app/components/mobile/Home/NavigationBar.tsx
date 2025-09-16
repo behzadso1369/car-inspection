7c9d3ecb7252ed9754942b59c55cc72d89327816
Soheilian.Behzad@okco.com <50904936+behzadso1369@users.noreply.github.com>
2025-09-16 22:00:05 +0330

    navbar has been finished

--- src/app/components/mobile/Home/NavigationBar.tsx
+++ src/app/components/mobile/Home/NavigationBar.tsx
@@ -1,26 +1,27 @@
 import { Home01Icon } from "hugeicons-react"
+import Image from "next/image"
 import Link from "next/link"
 
 export const NavigationBar = () => {
     return (
 
 
-        <nav className="fixed w-11/12 left-1/2 transform -translate-x-1/2 bottom-12 h-16 px-8 bg-[#f6f6f6] rounded-2xl shadow-[0px_4px_8px_0px_#00000014] flex justify-between items-center">
+        <nav className="fixed font-IranSans w-11/12 left-1/2 transform -translate-x-1/2 bottom-12 h-16 px-8 bg-[#f6f6f6] rounded-2xl shadow-[0px_4px_8px_0px_#00000014] flex justify-between items-center">
         <Link className="flex flex-col text-[#999A9C] active:text-[#416CEA]" href="/">
         <Home01Icon size={24}/>
-        <span>خانه</span>
+        <span className="text-xs my-1">خانه</span>
         </Link>
-        <Link className="flex flex-col text-[#999A9C] active:text-[#416CEA]" href="/flow">
-        <Home01Icon size={24}/>
-        <span>خانه</span>
+        <Link className="flex flex-col items-center justify-between text-[#999A9C] active:text-[#416CEA]" href="/flow">
+        <Image alt="کارشناسی خودرو" src="/car-inspection.svg" width={24} height={24}/>
+        <span className="text-xs my-1">کارشناسی خودرو</span>
         </Link>
-        <Link className="flex flex-col text-[#999A9C] active:text-[#416CEA]" href="/test">
-        <Home01Icon size={24} />
-        <span>خانه</span>
+        <Link className="flex flex-col   justify-between items-center text-[#999A9C] active:text-[#416CEA]" href="/test">
+        <Image alt="کارشناسی خودرو" src="/car-service.svg" width={24} height={24}/>
+        <span className="text-xs my-1">خدمات کارچک</span>
         </Link>
-        <Link className="flex flex-col text-[#999A9C] active:text-[#416CEA]" href="/test">
-        <Home01Icon size={24}/>
-        <span>خانه</span>
+        <Link className="flex flex-col justify-between items-center text-[#999A9C] active:text-[#416CEA]" href="/test">
+        <Image alt="کارشناسی خودرو" src="/profile.svg" width={24} height={24}/>
+        <span className="text-xs my-1">پروفایل</span>
         </Link>
         </nav>
     )
