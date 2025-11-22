e451ae415fc1225286b29361bb5d274ad04f6c5e
Soheilian.Behzad@okco.com <50904936+behzadso1369@users.noreply.github.com>
2025-11-22 23:53:06 +0330

    test all

--- src/app/components/mobile/Home/CallAction.tsx
+++ src/app/components/mobile/Home/CallAction.tsx
@@ -8,14 +8,14 @@ export default function CallAction ({data}:any) {
         <section className=" top-11 bg-secondary p-4 h-11 text-black flex justify-between items-center sticky z-10     shadow-[0px_4px_8px_0px_#00000014]">
             <div className="flex items-center">
             <Image alt="کارچک" width={32} height={30} src={"/assets/images/logo.svg"}/>
-            <h1 className="font-IranSans-UltraLight text-xl text-black mx-1 font-semibold">کارچک</h1>
+            <h1 className="font-IranSans-UltraLight text-xl text-black mx-1 font-semibold">{data?.MasterSiteData?.CompanyName}</h1>
             </div>
             
             <span className="text-[#101117] flex items-center font-IranSans">
                 
-                {data?.split("-").map((item:any) => (
-                     <a  className="rounded-3xl font-IranSans-UltraLight border border-white px-2" href={`tel:${item}`}>{item}</a>
-                ))}
+                
+                     <a  className="rounded-3xl font-IranSans-UltraLight border border-white px-2" href={`tel:${data?.MasterSiteData?.NavbarPhoneNumber}`}>{data?.MasterSiteData?.NavbarPhoneNumber}</a>
+                
                 <Call02Icon size={16}/>
             </span>
                 
